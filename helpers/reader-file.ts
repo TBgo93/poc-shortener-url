3a4aad6c2e780cc8ebaa78a69de232e9f417a73b
TBgo93 <tbaigorria93@gmail.com>
2024-04-27 16:53:28 -0300

    Refactor file mgmt

---
+++ helpers/reader-file.ts
@@ -0,0 +1,21 @@
+import { JsonDB } from "../types/common.d.ts";
+
+const FILE_PATH = "./db.json"
+
+const readerFile = async () => {
+  const text = await Deno.readTextFile(FILE_PATH);
+  const jsonFile: JsonDB = JSON.parse(text)
+  const { urls } = jsonFile;
+  return urls
+}
+
+const writterFile = async (urls: JsonDB["urls"]) => {
+  await Deno.writeTextFile(FILE_PATH, JSON.stringify({ urls: urls}));
+}
+
+const FILE = {
+  reader: readerFile,
+  writter: writterFile
+}
+
+export { FILE }
